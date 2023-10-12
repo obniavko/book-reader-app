55cb5f437b1af9a2752efeec05e2d16373e9761f
obniavko <vobniavko@gmail.com>
2023-10-12 23:28:38 +0300

    feat: add Book model validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ApplicationRecord
+  validates :title, :author, :description, presence: true
+  validates :title, length: { maximum: 150 }
+  validates :description, length: { maximum: 1000 }
+  validates :isbn, uniqueness: true
+  validates :isbn, format: { with: /\A[\d-]*\z/,
+    message: "only allows numbers and dashes" }
 end
