c09b6cf438c00a80b43c5c545824b9d0f9f0cdec
obniavko <vobniavko@gmail.com>
2023-10-13 17:50:54 +0300

    feat: install Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+20.times do
+  book = Book.new(
+  title: Faker::Book.title,
+  author: Faker::Book.author,
+  isbn: Faker::Code.isbn(base: 13),
+  description: "#{Faker::Book.genre}, #{Faker::Book.publisher}"
+  )
+
+  book.save
+end
