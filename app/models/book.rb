e6420305414672e4843f6b370f3ad02fc4ad0232
obniavko <vobniavko@gmail.com>
2023-10-13 23:38:18 +0300

    refactor: update format validation for isbn attribute

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   validates :title, presence: true, length: { maximum: 150 }
   validates :author, presence: true
-  validates :isbn, uniqueness: true, format: { with: /\A[\d-]*\z/,
+  validates :isbn, uniqueness: true, format: { with: /\A\d[\d-]*\d\z/,
     message: "only allows numbers and dashes" }
   validates :description, presence: true, length: { maximum: 1000 }
 end
