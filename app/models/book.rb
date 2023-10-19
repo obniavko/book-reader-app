f4f1352ed0a092eb764084cb8fc4733d9cd82a81
obniavko <vobniavko@gmail.com>
2023-10-19 14:38:08 +0300

    chore: added scope to the Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+  scope :ordered, -> { order(title: :asc) }
+
   validates :title, presence: true, length: { maximum: 150 }
   validates :author, presence: true
   validates :isbn, uniqueness: true, format: { with: /\A\d[\d-]*\d\z/,
