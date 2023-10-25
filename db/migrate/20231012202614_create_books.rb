b3eae99a0cf5e8be73a2473787c5fd90e98adfb3
obniavko <vobniavko@gmail.com>
2023-10-25 10:53:50 +0300

    chore: run db migration

--- db/migrate/20231012202614_create_books.rb
+++ db/migrate/20231012202614_create_books.rb
@@ -1,10 +1,10 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
-      t.string :title
-      t.string :author
+      t.string :title, null: false
+      t.string :author, null: false
       t.string :isbn
-      t.text :description
+      t.text :description, null: false
 
       t.timestamps
     end
--- db/migrate/20231016233527_make_columns_not_null.rb
+++
@@ -1,7 +0,0 @@
-class MakeColumnsNotNull < ActiveRecord::Migration[7.0]
-  def up
-    change_column_null :books, :title, false
-    change_column_null :books, :author, false
-    change_column_null :books, :description, false
-  end
-end
