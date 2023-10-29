43defe11bcc975c5fc8627333e8cfc73ae61230f
obniavko <vobniavko@gmail.com>
2023-10-29 20:19:43 +0200

    feat: add allow_blank option to isbn uniqueness validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
   validates :title, presence: true, length: { maximum: 150 }
   validates :author, presence: true
   validates :isbn, uniqueness: true, format: { with: /\A\d[\d-]*\d\z/,
-    message: "only allows numbers and dashes" }
+    message: "only allows numbers and dashes" }, allow_blank: true
   validates :description, length: { maximum: 1000 }
 
   scope :ordered, -> { order(:title) }
