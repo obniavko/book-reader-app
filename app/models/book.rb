558e3bea9e4f5c9da5f3811dbb7449f345ec2a08
obniavko <vobniavko@gmail.com>
2023-10-29 17:04:36 +0200

    refactor: move scope to the bottom

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,9 @@
 class Book < ApplicationRecord
-  scope :ordered, -> { order(:title) }
-
   validates :title, presence: true, length: { maximum: 150 }
   validates :author, presence: true
   validates :isbn, uniqueness: true, format: { with: /\A\d[\d-]*\d\z/,
     message: "only allows numbers and dashes" }
   validates :description, presence: true, length: { maximum: 1000 }
+
+  scope :ordered, -> { order(:title) }
 end
