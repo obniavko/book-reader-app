28f0f789118ddb0ca74917574519bd8c2cc69937
obniavko <vobniavko@gmail.com>
2023-10-30 23:22:10 +0200

    feat: add validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 150 }
-  validates :author, presence: true
+  validates :title, presence: true, length: { in: 1..150 }
+  validates :author, presence: true, length: { in: 1..50 }
   validates :isbn, uniqueness: true, format: { with: /\A\d[\d-]*\d\z/,
     message: "only allows numbers and dashes" }, allow_blank: true
   validates :description, length: { maximum: 1000 }
