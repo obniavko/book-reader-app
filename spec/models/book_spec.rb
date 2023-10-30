2524dabb850ae282bd6ccf869cb691bc2a62312f
obniavko <vobniavko@gmail.com>
2023-10-30 17:05:45 +0200

    test: replace should with is_expected.to, remove presence test for description field

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,18 +3,16 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_presence_of(:author) }
-  it { is_expected.to validate_presence_of(:description) }
   it { is_expected.to validate_length_of(:title).is_at_most(150) }
   it { is_expected.to validate_length_of(:description).is_at_most(1000) }
   it { is_expected.to allow_value("978-3-16-148410-0").for(:isbn) }
   it { is_expected.not_to allow_value("").for(:title) }
   it { is_expected.not_to allow_value("").for(:author) }
-  it { is_expected.not_to allow_value("").for(:description) }
   it { is_expected.not_to allow_value("isbn").for(:isbn) }
 
   describe "uniqueness" do
     subject { FactoryBot.create(:book) }
 
-    it { should validate_uniqueness_of(:isbn).case_insensitive }
+    it { is_expected.to validate_uniqueness_of(:isbn).case_insensitive }
   end
 end
