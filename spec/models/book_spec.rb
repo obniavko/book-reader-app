0376953a729c3381b30503e11a25c3a6847fb05d
obniavko <vobniavko@gmail.com>
2023-11-02 11:56:54 +0200

    test: refactor book_spec.rb

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
+  let!(:book) { FactoryBot.create(:book) }
+
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_presence_of(:author) }
   it { is_expected.to validate_presence_of(:isbn) }
@@ -9,9 +11,5 @@ RSpec.describe Book, type: :model do
   it { is_expected.to validate_length_of(:author).is_at_least(1).is_at_most(50) }
   it { is_expected.to validate_length_of(:description).is_at_most(1000) }
 
-  describe "uniqueness" do
-    subject { FactoryBot.create(:book) }
-
-    it { is_expected.to validate_uniqueness_of(:isbn).case_insensitive }
-  end
+  it { is_expected.to validate_uniqueness_of(:isbn).case_insensitive }
 end
