a64ed0f180e2e09921221ea32e2064de1fc6143f
obniavko <vobniavko@gmail.com>
2023-11-02 11:33:29 +0200

    feat: update isbn validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,7 @@
 class Book < ApplicationRecord
   validates :title, presence: true, length: { in: 1..150 }
   validates :author, presence: true, length: { in: 1..50 }
-  validates :isbn, presence: true, uniqueness: true,
-    format: { with: /\A\d[\d-]*\d\z/ }, allow_blank: true
+  validates :isbn, presence: true, uniqueness: true, format: { with: /\A\d[\d-]*\d\z/ }
   validates :description, length: { maximum: 1000 }
 
   scope :ordered, -> { order(:title) }
