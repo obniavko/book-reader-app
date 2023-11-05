bf30580db4e71e28f37c3752b5fe6857d7aacb8b
obniavko <vobniavko@gmail.com>
2023-11-05 17:26:59 +0200

    feat: update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true, length: { minimum: 6 }
+  validates :username, presence: true, uniqueness: true
+  validates :firstname, presence: true
+  validates :lastname, presence: true
 end
