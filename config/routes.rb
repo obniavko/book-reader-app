ff50797937dd9b6738653bfad66327dee15ebd5e
obniavko <vobniavko@gmail.com>
2023-11-07 00:02:44 +0200

    feat: limit Users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "books#index"
 
   resources :books
+  devise_for :users, skip: [:password, :registrations ]
+
+  as :user do
+    get "users/sign_up", to: "devise/registrations#new", as: :new_user_registration
+    post "users", to: "devise/registrations#create", as: :user_registration
+  end
 end
