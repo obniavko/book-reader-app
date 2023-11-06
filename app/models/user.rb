37106056b5cf16dc3f1eb8f468b3a3dd003ea783
obniavko <vobniavko@gmail.com>
2023-11-06 20:57:27 +0200

    feat: add email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable, :validatable
 
-  validates :email, presence: true, uniqueness: true
-  validates :encrypted_password, presence: true, length: { minimum: 6 }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+  validates :password, presence: true, length: { minimum: 6 }
   validates :username, presence: true, uniqueness: true
   validates :firstname, presence: true
   validates :lastname, presence: true
