97ac44b84daa99bc584081ac3a30617d80dfe83a
obniavko <vobniavko@gmail.com>
2023-11-06 20:59:05 +0200

    test: add User factories and tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(min_length: 6) }
+    username { Faker::Internet.username }
+    firstname { Faker::Name.first_name }
+    lastname { Faker::Name.last_name}
+  end
+
+  trait :short_password do
+    title { "12345" }
   end
 end
