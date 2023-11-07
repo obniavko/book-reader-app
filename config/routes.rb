4b0986e4fbd5001f44990dab4a75354408811633
obniavko <vobniavko@gmail.com>
2023-11-07 14:15:59 +0200

    refactor: update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   root "books#index"
 
   resources :books
-  devise_for :users, skip: [:password, :registrations ]
 
-  as :user do
-    get "users/sign_up", to: "devise/registrations#new", as: :new_user_registration
-    post "users", to: "devise/registrations#create", as: :user_registration
-  end
+  devise_for :users, skip: [:passwords, :sessions, :registrations]
+
+    devise_scope :user do
+      get 'users/sign_up', to: 'devise/registrations#new', as: :new_user_registration
+      post 'users', to: 'devise/registrations#create', as: :user_registration
+      get 'users/sign_in', to: 'devise/sessions#new', as: :new_user_session
+      post 'users/sign_in', to: 'devise/sessions#create', as: :user_session
+      delete 'users/sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+    end
 end
