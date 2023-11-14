b6c7da739bdb89d901cd179b526836b6b9e32d38
obniavko <vobniavko@gmail.com>
2023-11-14 23:52:00 +0200

    test: update Book factories and  model tests

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -13,4 +13,24 @@ FactoryBot.define do
   trait :new_title do
     title { "New Title" }
   end
+
+  trait :with_attached_cover do
+    after(:create) do |book|
+      book.cover.attach(
+        io: File.open(Rails.root.join("spec", "factories", "attachments", "cover_placeholder.png")),
+                      filename: "cover_placeholder.png",
+                      content_type: "image/png"
+      )
+    end
+  end
+
+  trait :with_attached_content do
+    after(:create) do |book|
+      book.content.attach(
+        io: File.open(Rails.root.join("spec", "factories", "attachments", "Lorem_ipsum.pdf")),
+                      filename: "Lorem_ipsum.pdf",
+                      content_type: "application/pdf"
+      )
+    end
+  end
 end
